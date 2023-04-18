127148acf28b5a016997bac85f1061bbb959d1da
Durruti <88114770+Durruuti@users.noreply.github.com>
2023-04-18 12:42:56 +0200

    Upload v2 de la red neuronal
    Esta V2 ha sido mejorada utilizando Chat-GPT

--- app.py
+++ app.py
@@ -1,35 +1,50 @@
 import tensorflow as tf
 import numpy as np
+import matplotlib.pyplot as plt
 
-celsius = np.array([-40, -10, 0, 8, 15, 22, 38], dtype=float)
-fahrenheit = np.array([-40, 14, 32, 46, 49, 72, 100], dtype=float)
+# Generar más puntos para el dataset
+celsius = np.array([-40, -30, -20, -10, 0, 10, 20, 30, 40, 50], dtype=float)
+fahrenheit = (celsius * 1.8) + 32
 
-#capa = tf.keras.layers.Dense(units=1, input_shape=[1])
-#modelo = tf.keras.Sequential([capa])
-oculta1 = tf.keras.layers.Dense(units=1, input_shape=[1])
-oculta2 = tf.keras.layers.Dense(units=1)
-salida = tf.keras.layers.Dense(units=1)
-modelo = tf.keras.Sequential([oculta1, oculta2, salida])
+# Dividir el dataset en conjunto de entrenamiento y conjunto de prueba
+train_celsius = celsius[:8]
+train_fahrenheit = fahrenheit[:8]
+test_celsius = celsius[8:]
+test_fahrenheit = fahrenheit[8:]
 
+# Definir el modelo
+modelo = tf.keras.Sequential([
+    tf.keras.layers.Dense(units=4, input_shape=[1], activation='relu'),
+    tf.keras.layers.Dense(units=4, activation='relu'),
+    tf.keras.layers.Dense(units=1)
+])
+
+# Compilar el modelo
 modelo.compile(
     optimizer=tf.keras.optimizers.Adam(0.1),
-    loss='mean_squared_error' # Corregido el nombre de la función de pérdida
+    loss='mean_absolute_error'
 )
 
+# Entrenar el modelo
 print("Comenzando entrenamiento...")
-historial = modelo.fit(celsius, fahrenheit, epochs=1000, verbose=False) # Corregido el nombre del modelo
+historial = modelo.fit(train_celsius, train_fahrenheit, epochs=1000, verbose=False)
 print("Modelo entrenado!")
 
-import matplotlib.pyplot as plt
+# Evaluar el modelo en el conjunto de prueba
+loss = modelo.evaluate(test_celsius, test_fahrenheit)
+print("Pérdida en el conjunto de prueba: ", loss)
+
+# Hacer una predicción
+resultado = modelo.predict([100.0])
+print("El resultado es:", resultado[0][0], "fahrenheit")
+
+# Graficar la evolución de la pérdida durante el entrenamiento
 plt.xlabel("#Epoca")
-plt.ylabel("Magnitud de predida")
+plt.ylabel("Magnitud de pérdida")
 plt.plot(historial.history["loss"])
 
-print("Hagamos una prediccion")
-resultado = modelo.predict([100.0])
-print("El resultado es:" + str(resultado) + " fahrenheit:")
-
-print("Variables internas del modelo")
-print(oculta1.get_weights())
-print(oculta2.get_weights())
-print(salida.get_weights())
+# Imprimir las variables internas del modelo
+for capa in modelo.layers:
+    pesos, sesgos = capa.get_weights()
+    print("Pesos:", pesos)
+    print("Sesgos:", sesgos)
